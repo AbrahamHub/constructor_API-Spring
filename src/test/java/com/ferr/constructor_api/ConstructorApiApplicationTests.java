d84715b0fc7656dd7dec8d435f23f08206a5ee43
AbrahamHub <apple_abraham@hotmail.com>
2021-06-03 21:01:15 -0500

    #1

---
+++ src/test/java/com/ferr/constructor_api/ConstructorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ferr.constructor_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConstructorApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
