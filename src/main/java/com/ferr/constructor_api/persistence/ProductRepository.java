04c65e857dcf33acd750277a8e641662bcebc560
AbrahamHub <apple_abraham@hotmail.com>
2021-06-23 19:43:36 -0500

    api ready

---
+++ src/main/java/com/ferr/constructor_api/persistence/ProductRepository.java
@@ -0,0 +1,55 @@
+package com.ferr.constructor_api.persistence;
+
+import com.ferr.constructor_api.domain.Product;
+import com.ferr.constructor_api.domain.repository.ProductRepository;
+import com.ferr.constructor_api.persistence.crud.ProductoCrudRepository;
+import com.ferr.constructor_api.persistence.entity.Producto;
+import com.ferr.constructor_api.persistence.mapper.ProductMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+class ProductoRepository implements ProductRepository {
+    @Autowired
+    private ProductoCrudRepository productoCrudRepository;
+
+    @Autowired
+    private ProductMapper mapper;
+
+    @Override
+    public List<Product> getAll() {
+        List<Producto> productos = (List<Producto>) productoCrudRepository.findAll();
+        return mapper.toProducts(productos);
+    }
+
+    @Override
+    public Optional<List<Product>> getByCategory(int categoryId) {
+        List<Producto> productos = productoCrudRepository.findByIdCategoriaOrderByNombreAsc(categoryId);
+        return Optional.of(mapper.toProducts(productos));
+    }
+
+    @Override
+    public Optional<List<Product>> getScarseProducts(int quantity) {
+        Optional<List<Producto>> productos = productoCrudRepository.findByCantidadStockLessThanAndEstado(quantity, true);
+        return productos.map(prods -> mapper.toProducts(prods));
+    }
+
+    @Override
+    public Optional<Product> getProduct(int productId) {
+        return productoCrudRepository.findById(productId).map(producto -> mapper.toProduct(producto));
+    }
+
+    @Override
+    public Product save(Product product) {
+        Producto producto = mapper.toProducto(product);
+        return mapper.toProduct(productoCrudRepository.save(producto));
+    }
+
+    @Override
+    public void delete(int productId) {
+        productoCrudRepository.deleteById(productId);
+    }
+}
---
+++ src/main/java/com/ferr/constructor_api/web/controller/ProductController.java
@@ -0,0 +1,36 @@
+package com.ferr.constructor_api.web.controller;
+
+import com.ferr.constructor_api.domain.Product;
+import com.ferr.constructor_api.domain.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+@GetMapping()
+    public List<Product> getAll(){
+        return productService.getAll();
+    }
+@GetMapping("/{id}")
+    public Optional<Product> getProduct(@PathVariable("id") int productId){
+        return productService.getProduct(productId);
+    }
+    @GetMapping("/category/{categoryId}")
+    public Optional<List<Product>> getByCategory(@PathVariable("categoryId") int categoryId){
+        return productService.getByCategory(categoryId);
+    }
+    @PostMapping("/save")
+    public Product save(@RequestBody Product product) {
+        return productService.save(product);
+    }
+    @DeleteMapping("/delete/{id}")
+    public boolean delete(@PathVariable("id") int productId){
+        return productService.delete(productId);
+    }
+}
